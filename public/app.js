8dc0fc124952c6eea3b974744238465c911ec017
Paulo Cesar Ferreira de Mello <pcfmello@gmail.com>
2016-02-26 04:20:59 -0300

    Finalizada a API de Contatos (JavaScript)

--- public/app.js
+++ public/app.js
@@ -1,88 +1,83 @@
 //LOCAL STORAGE
 $(document).ready(function() {
-
 	//FUNÇÕES DO LOCALSTORAGE
 	var localStorageFunctions = (function() {
-
 		//NOME DO OBJETO QUE CONTEM OS DADOS NO LOCALSTORAGE
-		var _appName = 'contatoAppList';
-		
+		var _appName = 'contatoAppList';		
 		//DESEMPACOTA OS DADOS E RETORNA-OS
 		var _getLocalStorage = function() {
-			return localStorage.getItem(_appName);
+			return JSON.parse(localStorage.getItem(_appName));
 		};
-
 		//EMPACOTA OS DADOS E ENVIA PARA O LOCALSTORAGE
 		var _setLocalStorage = function(dados) {
 			localStorage.setItem(_appName, JSON.stringify(dados));
 		};
-
 		return {
 			getLocalStorage : _getLocalStorage,
 			setLocalStorage : _setLocalStorage
 		}
-
 	})();
-
-	//INSERE OS DADOS NO LOCALSTORAGE
-	var insert = function(email, pageName, url, accessDate) {		
-		JSON.parse(contatoAppList.push(JSON.stringify({
-			email: email,
-			pageName : pageName,
-			url : url,
-			accessDate : accessDate	
-		})));
-		return contatoAppList;
-	};
-
-	//ALTERA OS DADOS NO LOCALSTORAGE
-	var edit = function(index, email, pageName, url, accessDate) {
-		item = JSON.stringify({
-			email: email,
-			pageName : pageName,
-			url : url,
-			accessDate : accessDate	
-		});
-		contatoAppList.splice(index, 1, item);
-		return contatoAppList;
-		
-	}
-
-	var executeApi = function(email, pageName, url, accessDate) {
-
-		var index = -1;
-
-		/*contatoAppList = JSON.parse(contatoAppList);*/
-
-		for(var prop in contatoAppList) {
-			console.log(JSON.parse(contatoAppList[prop]));
-			if(JSON.parse(contatoAppList[prop]).pageName == pageName) {
-				index = prop;				
-			} 
+	//FUNÇÕES DA API DE CONTATOS
+	var apiContatos = (function() {
+		//INSERE OS DADOS NO LOCALSTORAGE
+		var _insert = function(email, pageName, url, accessDate) {			
+			var localStorageContent = {};
+			var obj = { email : email, pageList : [] };	
+			obj.pageList.push({ pageName : pageName, url : url, accessDate : accessDate });
+			localStorageContent = JSON.stringify(obj);
+			return localStorageContent;
+		};
+		//ALTERA OS DADOS NO LOCALSTORAGE
+		var _update = function(localStorageContent, email, pageName, url, accessDate) {
+			var obj = { email : email };
+			var list = JSON.parse(localStorageContent).pageList;
+			var index = -1;
+			list.forEach(function(item) {
+				if(item.pageName === pageName) {
+					index = list.indexOf(item);
+				}
+			});
+			//SE A PÁGINA JÁ EXISTIR, ENTÃO ALTERA. 
+			//SENÃO, ADICIONA.
+			if(index >= 0) {
+				list.splice(index, 1, { pageName : pageName, url : url, accessDate : accessDate });	
+			} else {
+				list.push({ pageName : pageName, url : url, accessDate : accessDate });
+			}
+			obj.pageList = list;
+			localStorageContent = JSON.stringify(obj);
+			return localStorageContent;
+		};
+		return {
+			insert : _insert,
+			update : _update
 		}
-		console.log(index);
-		
-		if(index == undefined) {
-			contatoAppList = insert(email, pageName, url, accessDate);
-
+	})();
+	//FUNÇÃO PRINCIPAL - EXECUTA TODA A REGRA
+	var executeApi = function(email) {
+		var localStorageContent = localStorageFunctions.getLocalStorage();
+		if(!localStorageContent) {
+		//SE NÃO HOUVER COOKIE
+			localStorageContent = apiContatos.insert(email, $('#pageTitle').html(), $(location).attr('href'), new Date());
 		} else {
-			contatoAppList = edit(index, email, pageName, url, accessDate);
-
+		//SE HOUVER COOKIE
+			if(email) {
+				localStorageContent = apiContatos.update(localStorageContent, email, $('#pageTitle').html(), $(location).attr('href'), new Date());	
+			} else {
+				localStorageContent = apiContatos.update(localStorageContent, JSON.parse(localStorageContent).email, $('#pageTitle').html(), $(location).attr('href'), new Date());	
+			}
 		}
-
-		localStorageFunctions.setLocalStorage(contatoAppList);
-		/*console.log('contatoAppList : ' + JSON.parse(localStorageFunctions.getLocalStorage()));*/
+		localStorageFunctions.setLocalStorage(localStorageContent);
+		console.log(localStorageFunctions.getLocalStorage());
 	};
 	
 	$('#btEnviar').click(function() {
-		
 		var contato = {
 			nome: $('#nome').val(),
 			telefone: $('#telefone').val(),
 			email: $('#email').val(),
 			assunto: $('#assunto').val(),			
 		};
-
 		$.ajax({
 	        url: "http://localhost:3000/contatos.json",
 	        type: "POST",
@@ -92,38 +87,16 @@ $(document).ready(function() {
 			        "nome":     $('#nome').val(),
 			        "telefone": $('#telefone').val(),
 			        "email":    $('#email').val(),
-			        "assunto" : $('#assunto').val()
+			        "assunto" : $('#assunto').val(),
+			        "paginas" : JSON.parse(localStorageFunctions.getLocalStorage()).pageList
 			    }
 			},
-
 	        datatype: 'json',
 	        success: function (data) {
-	            console.log('Enviando dados... ');
-				executeApi(data.email, $('#pageTitle').html(), $(location).attr('href'), new Date());
-				
+				executeApi(data.email);
 	        }
 	    });
 	});	
-
-	//BUSCA OS DADOS DO USUÁRIO NO LOCALSTORAGE
-	var contatoAppList = localStorageFunctions.getLocalStorage();
-
-	if(contatoAppList == null) {
-		contatoAppList = [];
-	}
-
-	console.log('contatoAppList : ' + contatoAppList);
-
-	executeApi('', $('#pageTitle').html(), $(location).attr('href'), new Date());
-
-	contatoAppList.forEach(function(item) {
-		console.log('----------------------------------------');
-		console.log('');
-		console.log('Email: ' + JSON.parse(item).email);
-		for(prop in JSON.parse(item).pages) {
-			console.log(prop + ': ' + JSON.parse(item).pages[prop]);			
-		}		
-	});
-
-	
+	//INICIALIZA A APLICAÇÃO
+	executeApi();
 });
